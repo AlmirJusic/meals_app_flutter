f97fe485f3f38eb97e7e127d50e5127c05073c47
Almir <almir_jusich_@hotmail.com>
2022-10-12 13:38:33 +0200

    first commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // const CategoriesMealsScreen(this.categoryId, this.categoryTitle, {super.key});
+
+  static const routeName = '/category-meals';
+
+  const CategoryMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(
+                id: categoryMeals[index].id,
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                affordability: categoryMeals[index].affordability,
+                complexity: categoryMeals[index].complexity,
+                duration: categoryMeals[index].duration);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
